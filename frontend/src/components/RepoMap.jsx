c4221fb1df858db30474e1a7487920cce57babec
Hashirkhan24 <khn24202@gmail.com>
2024-02-13 13:19:02 +0530

    Add RepoMetrics to repo-map

--- frontend/src/components/RepoMap.jsx
+++ frontend/src/components/RepoMap.jsx
@@ -1,18 +1,45 @@
 import { useSelector } from "react-redux";
 import { selectRepoData } from "../store/selectors";
-import TreeNode from "./TreeNode"; // Import the TreeNode component
+import TreeNode from "./TreeNode"; 
+import { Line } from "react-chartjs-2"; // Importing Line from react-chartjs-2
+
 
 function RepoMap() {
-    const { structure } = useSelector(selectRepoData);
+    const { structure, repoMetrics, commits } = useSelector(selectRepoData);
+    console.log(commits);
 
     return (
-        <div className="font-mono">
-            <div className="text-lg text-gray-900 mb-2">Repo Map</div>
-            <div className="font-md h-screen flex flex-col justify-start items-start overflow-y-auto p-4 bg-gradient-to-b from-green-100 via-white to-teal-100 text-lg max-w-72 w-full">
-                {structure.map((node, index) => (
-                    <TreeNode key={index} node={node} />
-                ))}
+        <div className="font-mono bg-gray-700  ">
+            <div className="p-2 shadow-md w-full max-w-full flex items-center justify-center bg-gray-800 mb-2">
+                <h1 className="font-semibold text-3xl text-white self-center">Repo Map</h1>
             </div>
+            <section className="flex gap-3 w-full max-w-full flex-col md:flex-row ">
+                <div className="font-md h-screen flex flex-col justify-start items-start overflow-y-auto p-4 bg-slate-800 text-lg md:max-w-72 w-full">
+                <h1 className="font-semibold text-xl text-white mb-2">Folder Structure</h1>
+                    {structure.map((node, index) => (
+                        <TreeNode key={index} node={node} />
+                    ))}
+                </div>
+                <div className="max-w-full w-full  bg-gray-800">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-2 mx-2">
+                        <div className="bg-slate-700 text-white border border-slate-200 shadow-md p-4 flex gap-2">
+                            <h2 className="text-lg">Stars:</h2>
+                            {repoMetrics.stars}
+                        </div>
+                        <div className="bg-slate-700 text-white border border-slate-200 shadow-md p-4 flex gap-2">
+                            <h2 className="text-lg">Forks:</h2>
+                            {repoMetrics.forks}
+                        </div>
+                        <div className="bg-slate-700 text-white border border-slate-200 shadow-md p-4 flex gap-2">
+                            <h2 className="text-lg">Watchers:</h2>
+                            {repoMetrics.watchers}
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <h2 className="text-lg text-white mb-2">Commit History</h2>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
--- frontend/src/components/TreeNode.jsx
+++ frontend/src/components/TreeNode.jsx
@@ -1,4 +1,6 @@
-import { FaFileCode, FaFolder } from 'react-icons/fa';
+/* eslint-disable react/prop-types */
+import { AiFillFolder,  } from 'react-icons/ai';
+import { BsFiletypeHtml, BsFiletypeCss, BsFiletypeJs, BsFiletypeJsx, BsFiletypeJson, BsFiletypeMd, BsFiletypeTsx, BsFileCodeFill } from 'react-icons/bs'
 
 const TreeNode = ({ node }) => {
     const renderChildNodes = (childNodes) => {
@@ -7,13 +9,29 @@ const TreeNode = ({ node }) => {
         ));
     };
 
+    const getFileIcon = (fileName) => {
+    const extension = fileName.split('.').pop();
+    // Define your file type to icon mapping here
+    const iconMapping = {
+      js: <BsFiletypeJs className="h-6 w-6 mr-1 text-green-500" />,
+      html: <BsFiletypeHtml className="h-6 w-6 mr-1 text-red-500" />,
+      css: <BsFiletypeCss className="h-6 w-6 mr-1 text-blue-500" />,
+      jsx: <BsFiletypeJsx className="h-6 w-6 mr-1 text-blue-500" />,
+      md: <BsFiletypeMd className="h-6 w-6 mr-1 text-blue-500" />,
+      json: <BsFiletypeJson className="h-6 w-6 mr-1 text-blue-500" />,
+      tsx: <BsFiletypeTsx className="h-6 w-6 mr-1 text-blue-500" />,
+      // Add more mappings as needed
+    };
+
+    return iconMapping[extension.toLowerCase()] || <BsFileCodeFill className="h-6 w-6 mr-1 text-gray-500" />;
+  };
 
   return (
     <div className="ml-4 mb-2">
       {node.type === 'dir' ? (
         <details>
-          <summary className="cursor-pointer text-gray-800 font-medium flex items-center justify-left gap-2">
-            <FaFolder className="h-6 w-6 mr-1 text-blue-400"/>
+          <summary className="cursor-pointer text-white font-medium flex items-center justify-left gap-2">
+            <AiFillFolder className="h-6 w-6 mr-1 text-blue-400"/>
             {node.name}</summary>
           <div className="ml-2">
             {renderChildNodes(node.structure)}
@@ -21,8 +39,9 @@ const TreeNode = ({ node }) => {
         </details>
       ) : (
         <div className="flex items-center">
-            <FaFileCode className="h-4 w-4 mr-1 text-gray-500" />
-          <a href={node.downloadUrl} target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-600">{node.name}</a>
+            {/* <FaFileCode className="h-4 w-4 mr-1 text-gray-500" /> */}
+            {getFileIcon(node.name)}
+          <a href={node.downloadUrl} target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600">{node.name}</a>
         </div>
       )}
     </div>
